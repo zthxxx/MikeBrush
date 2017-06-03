2ca5e34746300477a797799f068eb2bd280ecce3
zthxxx <zth_9451@qq.com>
2017-06-04 00:52:45 +0800

    Support simple multithead for brush

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
-import logging
+import queue
 import logsetting
 from conffor import conffor
-from mikecrm.mikecrm import Mikecrm
+from mikecrm.mikebrush import MikeBrush
 
 conf_file = './mikecrm.json'
 config = conffor.load(conf_file)
 target = config['mikecrm']
+thead_count = config['thead_count']
 
 proxy_file = './proxy.json'
-proxys = conffor.load(proxy_file)['data']
-
+proxys_list = conffor.load(proxy_file)['data']
 
 if __name__ == "__main__":
-    brush = Mikecrm(**target)
-    count = 0
-    for index, item in enumerate(proxys):
-        proxy = (item['type'], item['ip'], item['port'])
-        if brush.set_proxy(*proxy).submit():
-            count += 1
-            logging.info('Current successes count is %d/%d' % (count, index))
+    proxys = queue.Queue()
+    for item in proxys_list:
+        proxys.put(item)
+    brush = MikeBrush(target, proxys, thead_count)
+    brush.run()
---
+++ mikecrm/mikebrush.py
@@ -0,0 +1,42 @@
+import logging
+from threading import Thread
+from .mikecrm import Mikecrm
+
+class MikeBrush():
+    def __init__(self, target, proxys, count):
+        '''
+        Brush for voting on mike
+        :param target: {"page":"", "data":""}
+        :param proxys: Queue for {"type":"", "ip":"", "port":00}
+        :param count: number of threadings
+        '''
+        self.target = target
+        self.proxys = proxys
+        self.count = count
+        self.total = 0
+        self.votes = 0
+
+    def brush_schedule(self, index):
+        proxys = self.proxys
+        brush = Mikecrm(**self.target)
+        logging.info('Brush thead-%d : task started!' % index)
+        while not proxys.empty():
+            item = proxys.get_nowait()
+            self.total += 1
+            proxy = (item['type'], item['ip'], item['port'])
+            if brush.set_proxy(*proxy).submit():
+                self.votes += 1
+            logging.info('Current successes count is %d / %d' % (self.votes, self.total))
+        logging.info('Brush thead-%d : task complete!' % index)
+
+    def run(self, block=True):
+        tasks = []
+        for index in range(self.count):
+            task = Thread(name='Theading-%d'%(index+1), target=self.brush_schedule, args=(index,))
+            tasks.append(task)
+            task.start()
+        logging.info('Brush tasks all started!')
+        if block:
+            for task in tasks:
+                task.join()
+            logging.info('Brush tasks all complete!')
